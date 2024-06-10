7cd77b189734817d516178215bed54d2b7a712c2
zwelisha19 <zwelishasiwela2@gmail.com>
2024-06-10 14:03:02 +0200

    created variables exercise

---
+++ variables.js
@@ -0,0 +1,31 @@
+//Execise 1
+let name = "Thabang";
+let age = 25;
+let isStudent = true;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+//Execise 2
+let favoriteCoour = 'Blue';
+console.log(favoriteCoour)
+
+favoriteCoour = 'yellow'
+console.log(favoriteCoour)
+
+//Execise 3
+let num1 = 20;
+let num2 = 5;
+
+let sum,difference,product,quotient;
+
+sum = num1 + num2;
+difference = num1 - num2;
+product = num1 * num2;
+quotient = num1/num2;
+
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
